77a41ee0db746ac06fca25afbf97a2b6ec65c357
aleRizzolo <43937404+aleRizzolo@users.noreply.github.com>
2020-01-24 14:44:39 +0100

    modifica database

--- Dress-Store/Database/dresstore.sql
+++ Dress-Store/Database/dresstore.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: dressshop
+-- Host: localhost    Database: dressstore
 -- ------------------------------------------------------
 -- Server version	8.0.17
 
