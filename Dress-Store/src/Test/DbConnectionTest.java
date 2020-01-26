5cb9852e1f5d80af69e4e23855011e146ca80e48
cresci97 <davidecresci@alice.it>
2020-01-26 18:20:11 +0100

    Test per verificare la connessione al DB

---
+++ Dress-Store/src/Test/DbConnectionTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import model.DriverManagerConnectionPool;
+
+public class DbConnectionTest {
+
+    @Test
+    public void testgetConn() throws SQLException {
+    DriverManagerConnectionPool db = new DriverManagerConnectionPool();
+    Connection con = db.getConnection();
+    assertNotNull(con);
+      }
+	}
+
