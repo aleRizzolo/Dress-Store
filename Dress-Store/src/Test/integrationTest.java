85d3f5706906f7e3dd4712da5271385a53ea3bf5
aleRizzolo <43937404+aleRizzolo@users.noreply.github.com>
2020-02-01 15:28:01 +0100

    integration test Registrazione

--- Dress-Store/src/Test/integrationTest.java
+++ Dress-Store/src/Test/integrationTest.java
@@ -17,24 +17,66 @@ import org.junit.Test;
 
 
 public class integrationTest {
-
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\SeleniumDrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String baseUrl="http://localhost:8080/Dress-Store/login.jsp ";
-		//		String expectedTitle="LOGIN";
-		//		String actualTitle = "";
-				
-				driver.get(baseUrl);
-				WebElement email=driver.findElement(By.name("email"));
-		         WebElement pass=driver.findElement(By.name("password"));
-		         WebElement submitButton=driver.findElement(By.name("invio"));
-		         email.sendKeys("uno@due");
-		         pass.sendKeys("root");
-		         submitButton.click();
-		         String actualTitle = driver.getTitle();
-		         System.out.println("Test Failed");
-		      
-				 driver.close ();
-				 }
+        
+        driver.get(baseUrl);
+        WebElement email=driver.findElement(By.name("email"));
+		WebElement pass=driver.findElement(By.name("password"));
+		WebElement submitButton=driver.findElement(By.name("invio"));
+		email.sendKeys("uno@due");
+		pass.sendKeys("root");
+		submitButton.click();
+		String actualTitle = driver.getTitle();
+		System.out.println("Test Failed");
+		
+		//driver.close ();
+		
+		baseUrl="http://localhost:8080/Dress-Store/registrazione.jsp ";
+		driver.get(baseUrl);
+	         
+	         WebElement nome=driver.findElement(By.name("nome"));
+	         
+	         WebElement cognome=driver.findElement(By.name("cognome"));
+	         
+	         WebElement data=driver.findElement(By.name("data_nascita"));
+	         email=driver.findElement(By.name("email"));
+	         
+	         WebElement password=driver.findElement(By.name("password"));
+	          
+	         WebElement registrati=driver.findElement(By.name("regButtom"));
+	         nome.sendKeys("!4&");
+	         cognome.sendKeys("950");
+	         data.sendKeys("12/14/918");
+	         email.sendKeys("uno");
+	         password.sendKeys("pass");
+
+	         registrati.click();
+	         
+	         nome.clear();
+	         cognome.clear();
+	         data.clear();
+	         email.clear();
+	         password.clear();
+	         
+	         registrati.click();
+	         
+	         nome.sendKeys("nome");
+	         cognome.sendKeys("cognome");
+	         data.sendKeys("12/11/18");
+	         email.sendKeys("otto@otto.it");
+	         
+	         registrati.click();
+	         
+	         password.sendKeys("password");
+	         
+	         registrati.click();
+
+	         driver.close();
+	         
 	}
+ 
+}
